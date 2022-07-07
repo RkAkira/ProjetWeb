7dbc84c162604197d5f4ab0a399200036a84fb36
Saathik <77830488+Saathik@users.noreply.github.com>
2022-07-07 12:47:59 +0200

    add dashboard

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -47,7 +47,7 @@ function Dashboard() {
     return (
         <div class="">
             <div class="grid grid-cols-12 gap-6 mt-48 lg:mx-52">
-            <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+            <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-4 intro-y bg-white"
                     href="#">
                     <div class="p-5">
                         <div class="flex justify-between">
@@ -60,14 +60,14 @@ function Dashboard() {
                         </div>
                         <div class="ml-2 w-full flex-1">
                             <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{nbVentes}</div>
+                                <div class="mt-3 text-3xl font-bold leading-8">26</div>
 
                                 <div class="mt-1 text-base text-gray-600">Nombre vente</div>
                             </div>
                         </div>
                     </div>
                 </a>
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-4 intro-y bg-white"
                     href="#">
                     <div class="p-5">
                         <div class="flex justify-between">
@@ -83,35 +83,14 @@ function Dashboard() {
                         </div>
                         <div class="ml-2 w-full flex-1">
                             <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{total}</div>
-
+                                <div class="mt-3 text-3xl font-bold leading-8">13000</div>
                                 <div class="mt-1 text-base text-gray-600">Total Vente</div>
                             </div>
                         </div>
                     </div>
                 </a>
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                    href="#">
-                    <div class="p-5">
-                        <div class="flex justify-between">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-yellow-400"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                            </svg>
-                        </div>
-                        <div class="ml-2 w-full flex-1">
-                            <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{com}</div>
-
-                                <div class="mt-1 text-base text-gray-600">Total commission</div>
-                            </div>
-                        </div>
-                    </div>
-                </a>
                 
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-4 intro-y bg-white"
                     href="#">
                     <div class="p-5">
                         <div class="flex justify-between">
@@ -124,7 +103,7 @@ function Dashboard() {
                         </div>
                         <div class="ml-2 w-full flex-1">
                             <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{last}</div>
+                                <div class="mt-3 text-3xl font-bold leading-8">Console PS5 Edition Standard</div>
 
                                 <div class="mt-1 text-base text-gray-600">Dernière vente</div>
                             </div>
